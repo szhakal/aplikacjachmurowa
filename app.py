9ea59ddf4678c7a76415e77c700d8295dfdb3be5
Nikodem Rudzinski <nikodemrudzinski@MacBook-Pro-de-Nikodem.local>
2021-06-09 03:43:40 +0200

    dziala

--- app.py
+++ app.py
@@ -5,17 +5,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/contact')
-def contact():
-    return render_template("contact.html")
+@app.route('/contact3')
+def contact3():
+    return render_template("contact3.html")
     
 @app.route('/gallery')
 def gallery():
     return render_template('gallery.html')
 
-@app.route('/about')
+@app.route('/about1')
 def about():
-    return app.send_static_file('about.html')
+    return render_template('about1.html')
 if __name__ == '__main__':
     app.run(debug=True)
 
@@ -25,5 +25,13 @@ def error_not_found():
     response = make_response(render_template('template.html', name='ERROR 404'), 404) 
     response.headers['X-Something'] = 'A value'
     return response
+
+@app.route('/error_denied')
+def error_denied():
+    abort(401)
+
+@app.route('/error_internal')
+def error_internal():
+    return render_template('505.html'), 505
 
 
